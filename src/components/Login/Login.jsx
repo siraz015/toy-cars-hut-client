a34af15eb8bc87c47738a7d7ec0bd46172f94b74
siraz015 <siraj015213@gmail.com>
2023-05-18 19:34:59 +0600

    implement login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div className='w-1/3 mx-auto bg-gray-100 p-5 rounded-lg mt-10 shadow-lg'>
-            {/* <h1 className="text-5xl text-center font-bold my-5">Please Login!</h1>
+            <h1 className="text-5xl text-center font-bold my-5">Please Login!</h1>
             <form>
                 <div className="form-control">
                     <label className="label">
@@ -25,7 +25,7 @@ const Login = () => {
                     <input className="btn btn-primary" type="submit" value="Login" />
                 </div>
             </form>
-            <p className='my-4'><small>Don`t have account? <Link to='/register'>Please Register!</Link></small></p> */}
+            <p className='my-4'><small>Don`t have account? <Link to='/register'>Please Register!</Link></small></p>
         </div>
     );
 };

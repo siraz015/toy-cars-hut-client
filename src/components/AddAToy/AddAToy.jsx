ef0fc3945698a57c1fd34e6d891ca89208d677d3
siraz015 <siraj015213@gmail.com>
2023-05-20 00:23:30 +0600

    add toy into mongodb

--- src/components/AddAToy/AddAToy.jsx
+++ src/components/AddAToy/AddAToy.jsx
@@ -1,78 +1,120 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const AddAToy = () => {
+    const { user } = useContext(AuthContext);
+
+    const handleAddToy = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const pictureURL = form.pictureURL.value;
+        const name = form.name.value;
+        const sellerName = form.sellerName.value;
+        const availableQuantity = form.availableQuantity.value;
+        const subCategory = form.subCategory.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const detailDescription = form.detailDescription.value;
+
+        const addToy = {
+            pictureURL,
+            name,
+            sellerName,
+            availableQuantity,
+            subCategory,
+            price,
+            rating,
+            detailDescription
+        }
+
+        fetch('http://localhost:5000/addtoy', {
+            method:'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(addToy)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
+
     return (
         <div className='my-16 max-w-[1280px] mx-auto'>
             <h2 className='text-3xl font-bold text-center mb-5'>Please Add A toy</h2>
-            <div className='grid md:grid-cols-2 gap-5'>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Picture URL</span>
-                    </label>
-                    <input type="text" name='pictureURL' placeholder="Picture URL" className="input input-bordered" />
+            <form onSubmit={handleAddToy}>
+                <div className='grid md:grid-cols-2 gap-5'>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Picture URL</span>
+                        </label>
+                        <input type="text" name='pictureURL' placeholder="Picture URL" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name='name' placeholder="Toy Name" className="input input-bordered" />
+                    </div>
+                </div>
+                <div className='grid md:grid-cols-2 gap-5'>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Seller Name</span>
+                        </label>
+                        <input type="text" defaultValue={user?.displayName} name='sellerName' placeholder="Seller Name" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Seller Email</span>
+                        </label>
+                        <input type="text" name='sellerEmail' defaultValue={user?.email} placeholder="Seller Email" className="input input-bordered" />
+                    </div>
+                </div>
+                <div className='grid md:grid-cols-2 gap-5'>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Available Quantity</span>
+                        </label>
+                        <input type="text" name='availableQuantity' placeholder="Available Quantity" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Sub Category</span>
+                        </label>
+                        <select className="input input-bordered" name="subCategory" id="subCategory">
+                            <option value="Sports Car">Sports Car</option>
+                            <option value="Mini Fire Truck">Mini Fire Truck</option>
+                            <option value="Mini Plice Car">Mini Plice Car</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='grid grid-cols-2 gap-5'>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <input type="text" name='price' placeholder="Price" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Rating</span>
+                        </label>
+                        <input type="text" name='rating' placeholder="Rating" className="input input-bordered" />
+                    </div>
                 </div>
                 <div className="form-control">
                     <label className="label">
-                        <span className="label-text">Name</span>
+                        <span className="label-text">Detail description</span>
                     </label>
-                    <input type="text" name='name' placeholder="Name" className="input input-bordered" />
-                </div>
-            </div>
-            <div className='grid md:grid-cols-2 gap-5'>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Seller Name</span>
-                    </label>
-                    <input type="text" name='sellerName' placeholder="Seller Name" className="input input-bordered" />
+                    <input type="text" name='detailDescription' placeholder="Detail description" className="input input-bordered" />
                 </div>
                 <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Seller Email</span>
-                    </label>
-                    <input type="text" name='sellerEmail' placeholder="Seller Email" className="input input-bordered" />
+                    <button className='bg-blue-500 rounded-lg py-2 text-white font-bold my-5 btn btn-primary'>Submit</button>
                 </div>
-            </div>
-            <div className='grid md:grid-cols-2 gap-5'>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Available Quantity</span>
-                    </label>
-                    <input type="text" name='availableQuantity' placeholder="Available Quantity" className="input input-bordered" />
-                </div>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Sub Category</span>
-                    </label>
-                    <select className="input input-bordered" name="subCategory" id="subCategory">
-                        <option value="volvo">Sports Car</option>
-                        <option value="saab">Mini Fire Truck</option>
-                        <option value="opel">Mini Plice Car</option>
-                    </select>
-                </div>
-            </div>
-            <div className='grid grid-cols-2 gap-5'>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Price</span>
-                    </label>
-                    <input type="text" name='price' placeholder="Price" className="input input-bordered" />
-                </div>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Rating</span>
-                    </label>
-                    <input type="text" name='rating' placeholder="Rating" className="input input-bordered" />
-                </div>
-            </div>
-            <div className="form-control">
-                <label className="label">
-                    <span className="label-text">Detail description</span>
-                </label>
-                <input type="text" name='detailDescription' placeholder="Detail description" className="input input-bordered" />
-            </div>
-            <div className="form-control">
-                <button className='bg-blue-500 rounded-lg py-2 text-white font-bold my-5'>Submit</button>
-            </div>
+            </form>
         </div>
     );
 };

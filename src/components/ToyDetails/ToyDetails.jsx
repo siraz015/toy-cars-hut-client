fe7b21c66a76e1bd32c3e9c16bac8614e803e31c
siraz015 <siraj015213@gmail.com>
2023-05-21 14:58:45 +0600

    added toy details components

--- src/components/ToyDetails/ToyDetails.jsx
+++ src/components/ToyDetails/ToyDetails.jsx
@@ -3,16 +3,28 @@ import { useLoaderData } from 'react-router-dom';
 
 const ToyDetails = () => {
     const toy = useLoaderData();
-    const {_id, availableQuantity, detailDescription, name, pictureURL, price, rating, sellerName, subCategory} = toy;
-    
-    console.log(toy);
+    const { _id, availableQuantity, detailDescription, name, pictureURL, price, rating, sellerName, subCategory } = toy;
 
-    
 
     return (
         <div>
-            <h2>Toy Details Page</h2>
-            <img src={pictureURL} alt="" />
+            <div className='max-w-[1280px] mx-auto'>
+                <h2 className='text-center font-bold text-5xl mb-5 text-blue-800'> {name} Details </h2>
+                <div className='grid md:grid-cols-2 gap-10'>
+                    <div className=' shadow-xl rounded-xl'>
+                        <img className='max-w-md' src={pictureURL} alt="" />
+                    </div>
+                    <div className='flex flex-col justify-center shadow-xl rounded-xl p-10'>
+                        <h2 className='text-4xl font-bold mb-3'>Toy Name: {name}</h2>
+                        <p className='text-2xl font-semibold mb-2'>Category: {subCategory}</p>
+                        <p className='text-lg'>Details Description: {detailDescription}</p>
+                        <p className='text-lg'>Quantity: {availableQuantity}</p>
+                        <p>Ratings: {rating}</p>
+                        <p className='text-lg '>Price: {price}</p>
+                        <button className='btn btn-primary w-1/4 mt-5'>Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
